205760f4d1fba2080ea529c260a88e5169b0b4a2
ZPAA-123 <1401982916@qq.com>
2023-11-23 17:32:53 +0800

    项目基本架构

---
+++ gulimail-coupon/src/main/java/com/zpaa/gulimail/coupon/GulimailCouponApplication.java
@@ -0,0 +1,13 @@
+package com.zpaa.gulimail.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimailCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimailCouponApplication.class, args);
+    }
+
+}
